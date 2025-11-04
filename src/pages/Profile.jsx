66b043e1d56a90b3af2435798a948666327903d7
Thejan Dulara <thejangammanpila@gmail.com>
2025-11-04 17:19:21 +0530

    changes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { api } from "../api";
 import { useAuth } from "../AuthContext";
-import { toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Profile() {
